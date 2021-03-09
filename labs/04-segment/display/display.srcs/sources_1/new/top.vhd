e908a43ed55d3a2fd1bde150805eb33b4ac28add
MartinSomsak00 <xsomsakm@seznam.cz>
2021-03-09 20:08:04 +0100

    taks3

--- labs/04-segment/display/display.srcs/sources_1/new/top.vhd
+++ labs/04-segment/display/display.srcs/sources_1/new/top.vhd
@@ -69,6 +69,31 @@ architecture Behavioral of top is
 
     -- Display input value
     LED(3 downto 0) <= SW;
+     LED(4) <= '1' when (SW = "0000") else '0';
+    
+    -- Turn LED(5) on if input value is greater than "1001", ie 9
+    LED(5) <= '1' when (SW = "1010") else '0',
+              '1' when (SW = "1011") else '0',
+              '1' when (SW = "1100") else '0',
+              '1' when (SW = "1101") else '0',
+              '1' when (SW = "1110") else '0',
+              '1' when (SW = "1111") else '0';
+    
+    -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
+    LED(6) <= '1' when (SW = "0001") else '0',
+              '1' when (SW = "0011") else '0',
+              '1' when (SW = "0101") else '0',
+              '1' when (SW = "0111") else '0',
+              '1' when (SW = "1001") else '0',
+              '1' when (SW = "1011") else '0',
+              '1' when (SW = "1101") else '0',
+              '1' when (SW = "1111") else '0';
+    
+    -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
+    LED(7) <= '1' when (SW = "0001") else '0',
+              '1' when (SW = "0010") else '0',
+              '1' when (SW = "0100") else '0',
+              '1' when (SW = "1000") else '0';
 
 
 end Behavioral;
